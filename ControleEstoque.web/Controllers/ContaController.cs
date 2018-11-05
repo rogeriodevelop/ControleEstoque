02d90a6f011aa7485a7fae81d3f3dad3101c67c0
Rogerio Santos <rogeriodevelop@gmail.com>
2018-11-05 00:57:56 -0200

    Post do login e Display

--- ControleEstoque.web/Controllers/ContaController.cs
+++ ControleEstoque.web/Controllers/ContaController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControleEstoque.web.Models;
 
 namespace ControleEstoque.web.Controllers
 {
     public class ContaController : Controller
     {
         [AllowAnonymous]
-        public ActionResult Login(String returnUrl)
+        public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            return View(login);
         }
     }
 }
--- ControleEstoque.web/Models/LoginViewModel.cs
+++ ControleEstoque.web/Models/LoginViewModel.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleEstoque.web.Models
 {
     public class LoginViewModel
     {
+        [Display (Name ="Usuário: ") ]
         public String Usuario { get; set; }
+
+        [Display(Name = "Senha: ")]
         public String Senha { get; set; }
+
+        [Display(Name = "Lembrar-me")]
         public bool LembrarMe { get; set; }
     }
+
 }
